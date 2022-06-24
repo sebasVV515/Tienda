69cc00b89cf3149e3a4a2d4e83a470cecf0150ec
julianarce1995 <100883327+julianarce1995@users.noreply.github.com>
2022-06-24 12:34:02 -0500

    Primera parte index

---
+++ src/controllers/controladorIndex.js
@@ -0,0 +1,6 @@
+let btnCambio = document.getElementById("cambio")
+let imagen = document.getElementById("imagen")
+let titulo = document.getElementById("titulo")
+
+titulo.textContent="Chao"
+imagen.src = "https://placeimg.com/640/480/any"
