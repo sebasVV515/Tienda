a35f0a49b26b61cfc8a97c3aae8ff7f66284e158
julianarce1995 <100883327+julianarce1995@users.noreply.github.com>
2022-07-01 12:15:32 -0500

    agregar tienda

---
+++ src/controllers/controladorllenado.js
@@ -0,0 +1,51 @@
+let peliculas = [{
+    Nombre: "Shark Bait",
+    Precio: 125000,
+    Descripcion: "Un grupo de amigos que disfrutan de un fin de semana roban un par de motos acuáticas que los llevan al mar y terminan en una horrible colisión frontal. Luchan por encontrar el camino a casa con un amigo gravemente herido mientras los depredadores acechan en las aguas debajo.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/sharkbait.jpg?alt=media&token=7883b1a6-3f2c-4100-b166-e96634148bd2","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/sharkbait.jpg?alt=media&token=7883b1a6-3f2c-4100-b166-e96634148bd2"]
+},{
+    Nombre: "The Lost City",
+    Precio: 235000,
+    Descripcion: "Una solitaria novelista romántica de gira con el modelo de la portada de su último libro se ve envuelta en un intento de secuestro que llevará a ambos a una feroz aventura en la jungla.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/thelostcity.jpg?alt=media&token=523a5e43-215e-4645-b21f-c0019cd82f78","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/thelostcity.jpg?alt=media&token=523a5e43-215e-4645-b21f-c0019cd82f78"]
+},{
+    Nombre: "The Northman",
+    Precio: 45000,
+    Descripcion: "El príncipe Amleth está a punto de convertirse en hombre pero, en ese momento, su tío asesina brutalmente a su padre y secuestra a la madre del niño. Dos décadas después, Amleth es un vikingo que tiene la misión de salvar a su madre.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/thenorthman.jpg?alt=media&token=01d3a8f2-0c00-43e1-8da1-3a8f195a3a7e","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/thenorthman.jpg?alt=media&token=01d3a8f2-0c00-43e1-8da1-3a8f195a3a7e"]
+},{
+    Nombre: "Senior Year",
+    Precio: 57000,
+    Descripcion: "Una mujer de treinta y siete años se despierta de un coma de veinte años y regresa a la escuela secundaria donde una vez fue una popular animadora.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/senioryear.jpg?alt=media&token=6b4f0cda-8bf3-4c50-9a9a-6e896f4c16b7","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/senioryear.jpg?alt=media&token=6b4f0cda-8bf3-4c50-9a9a-6e896f4c16b7"]
+},{
+    Nombre: "Firestarter",
+    Precio: 64000,
+    Descripcion: "Andy y Vicky son unos padres que llevan más de una década huyendo en un desesperado intento de esconder a su hija Charlie de una oscura agencia estadounidense empeñada en aprovechar su increíble don para convertir el fuego en un arma de destrucción masiva. Andy enseñó a Charlie cómo dominar su extraño poder, desencadenado normalmente por la ira o el dolor. Pero cuando Charlie cumple 11 años, el fuego es cada vez más difícil de controlar. Después de que un incidente inesperado revele la ubicación de la familia, un misterioso operario sale en busca de Charlie para hacerse con ella de una vez por todas. Pero Charlie tiene otros planes.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/firestarter.jpg?alt=media&token=6db09d60-0525-464b-b833-5e144d04147f","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/firestarter.jpg?alt=media&token=6db09d60-0525-464b-b833-5e144d04147f"]
+},{
+    Nombre: "The Batman",
+    Precio: 125000,
+    Descripcion: "Cuando un asesino se dirige a la élite de Gotham con una serie de maquinaciones sádicas, un rastro de pistas crípticas envía Batman a una investigación en los bajos fondos. A medida que las pruebas comienzan a acercarse a su casa y se hace evidente la magnitud de los planes del autor, Batman debe forjar nuevas relaciones, desenmascarar al culpable y hacer justicia al abuso de poder y la corrupción que durante mucho tiempo han asolado Gotham City.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/batman.jpg?alt=media&token=947304fc-c79c-48b5-aba9-4bda34237701","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/batman.jpg?alt=media&token=947304fc-c79c-48b5-aba9-4bda34237701"]
+},{
+    Nombre: "Uncharted",
+    Precio: 235000,
+    Descripcion: "Un descendiente del explorador Sir Francis Drake descubre la ubicación de la legendaria ciudad de El Dorado. Con la ayuda de su mentor Victor Sullivan y la ambiciosa periodista Elena Fischer, Nathan Drake trabajará para descubrir sus secretos, mientras sobreviven en una isla llena de piratas, mercenarios, y un misterioso enemigo, se embarcarán en una búsqueda sin precedentes por alcanzar el tesoro antes que sus perseguidores. Adaptación del aclamado videojuego homónimo.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/uncharted.jpg?alt=media&token=e8f425eb-a9c4-4e8d-a7fa-2ca804a47ddb","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/uncharted.jpg?alt=media&token=e8f425eb-a9c4-4e8d-a7fa-2ca804a47ddb"]
+},{
+    Nombre: "Moonfall",
+    Precio: 45000,
+    Descripcion: "El mundo se enfrenta a la posibilidad de la extinción porque una fuerza desconocida empuja a la luna de su órbita rumbo a la Tierra. Un equipo de astronautas asume la misión de evitar el apocalipsis.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/moonfall.jpg?alt=media&token=c7a85f76-3472-4249-9826-d1e0ae9f59a4","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/moonfall.jpg?alt=media&token=c7a85f76-3472-4249-9826-d1e0ae9f59a4"]
+},{
+    Nombre: "Scream",
+    Precio: 57000,
+    Descripcion: "Una nueva entrega de la saga de terror 'Scream' que seguirá a una mujer que regresa a su ciudad natal para intentar descubrir quién ha estado cometiendo una serie de crímenes atroces.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/scream.jpg?alt=media&token=d9db34fe-9184-411d-827a-24965e0061a8","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/scream.jpg?alt=media&token=d9db34fe-9184-411d-827a-24965e0061a8"]
+},{
+    Nombre: "Jackass Forever",
+    Precio: 64000,
+    Descripcion: "Celebre la alegría de un tiro perfectamente ejecutado en la ingle cuando Johnny Knoxville, Steve-O y el resto de la pandilla regresan junto con algunos recién llegados para una ronda final de exhibiciones de acrobacias y comedia hilarantes, tremendamente absurdas y, a menudo, peligrosas.",
+    Imagenes: ["https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/jackass.jpg?alt=media&token=4eac8f6c-9ec7-4923-87ba-d265ab89ecab","https://firebasestorage.googleapis.com/v0/b/svtienda-941c4.appspot.com/o/jackass.jpg?alt=media&token=4eac8f6c-9ec7-4923-87ba-d265ab89ecab"]
+}]
